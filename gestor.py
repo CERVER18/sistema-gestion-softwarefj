9e824300fb9b2c86f90816e9b902f3166eda4d0e
CERVER18 <coencisob@unadvirtual.edu.co>
2026-05-10 01:03:19 -0500

    Gestiona las listas internas

--- gestor.py
+++ gestor.py
@@ -1,1 +1,15 @@
+from logger import registrar_log
 
+class GestorSistema:
+    """Clase para manejar las listas internas sin base de datos."""
+    def __init__(self):
+        self.clientes = []
+        self.servicios = []
+        self.reservas_historial = []
+
+    def agregar_cliente(self, cliente):
+        self.clientes.append(cliente)
+        registrar_log(f"Cliente añadido a la lista: {cliente.nombre}")
+
+    def registrar_operacion(self, reserva):
+        self.reservas_historial.append(reserva)
