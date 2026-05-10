231e4c2a60ac75dccdd9f4b7351aea162f80340d
CERVER18 <coencisob@unadvirtual.edu.co>
2026-05-10 01:00:11 -0500

    Implementa la abstracción base.
    Implementa la abstracción base.

--- entidad_base.py
+++ entidad_base.py
@@ -1,1 +1,10 @@
+from abc import ABC
 
+class EntidadSistema(ABC):
+    """Clase abstracta que define la identidad de los objetos."""
+    def __init__(self, id_entidad):
+        self._id_entidad = id_entidad  # Atributo protegido
+
+    @property
+    def id_entidad(self):
+        return self._id_entidad
