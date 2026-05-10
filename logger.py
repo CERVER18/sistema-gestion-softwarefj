c5966afd2316a05d5639af8e2f31157bdbf32739
CERVER18 <coencisob@unadvirtual.edu.co>
2026-05-10 00:59:03 -0500

    Update logger.py
    Configura el registro de eventos en archivos físicos.

--- logger.py
+++ logger.py
@@ -1,1 +1,19 @@
+import logging
+import os
 
+# Aseguramos que la carpeta logs exista
+if not os.path.exists('logs'):
+    os.makedirs('logs')
+
+logging.basicConfig(
+    filename='logs/sistema_fj.log',
+    level=logging.INFO,
+    format='%(asctime)s | %(levelname)s | %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+def registrar_log(mensaje, nivel="info"):
+    if nivel == "info": logging.info(mensaje)
+    elif nivel == "error": logging.error(mensaje)
+    elif nivel == "warning": logging.warning(mensaje)
+    elif nivel == "critical": logging.critical(mensaje)
